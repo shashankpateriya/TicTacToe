c4bdae7e70c166cd34190fc8c2cb183b1b98eaa7
Shashank <shashankpateriya4@gmail.com>
2021-07-21 11:57:35 +0530

    [shashank] Add: Creating array of space

---
+++ Game.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TicTacToe
+{
+    class Game
+    {
+        public char[] Board()
+        {
+            char[] createBoard = new char[10];
+            for (int i = 1; i < createBoard.Length; i++)
+            {
+                createBoard[i] = ' ';
+            }
+            return createBoard;
+        }
+    }
+}
