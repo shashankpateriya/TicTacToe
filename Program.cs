027511a66861c5886f6daef409468a8bb185abb0
Shashank <shashankpateriya4@gmail.com>
2021-07-21 11:56:36 +0530

    [shashank] Add: Program main method

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TicTacToe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game obj = new Game();
+            obj.Board();
+        }
+    }
+}
